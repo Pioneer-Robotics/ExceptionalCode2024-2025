7711f6fb344dcc25024d786ba2e67b67dea5c8f6
nwazus <nwazus@gmail.com>
2024-06-17 14:55:47 -0700

    Started working on color detection pipeline.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Camera/CameraHandler.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.Hardware.Camera;
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+@TeleOp
+public class CameraHandler extends LinearOpMode {
+    OpenCvWebcam webcam;
+
+    @Override
+    public void runOpMode(){
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webc" +
+                "am 1"), cameraMonitorViewId);
+
+        webcam.setPipeline(new ColorDetectionPipeline());
+        webcam.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener(){
+            @Override
+            public void onOpened(){
+                webcam.startStreaming(1280,720,OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode){
+                //Error idk
+            }
+        });
+
+        waitForStart();
+        while(opModeIsActive()){
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+//            telemetry.addData("Location", )
+            telemetry.update();
+
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Camera/ColorDetectionPipeline.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.Hardware.Camera;
+
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class ColorDetectionPipeline extends OpenCvPipeline{
+    //Streaming in 1280x720
+    //
+    public int width = 1280;
+    public int height = 720;
+
+    Scalar lowerRed = new Scalar(108,40,40); // Adjust as needed for light blue
+    Scalar upperRed = new Scalar(255,0,0); // Adjust as needed for dark blue
+
+    Rect leftHalf = new Rect(1,1,(width/2),height-1);
+    Rect rightHalf = new Rect((width/2)+1,1,width-2,height-1);
+
+    Mat blurredImg = new Mat();
+    Mat modImg = new Mat();
+    Mat leftCrop = new Mat();
+    Mat rightCrop = new Mat();
+
+    public static int location = -1;
+
+    double leftAvgFin;
+
+    double rightAvgFin;
+
+
+    @Override
+    public Mat processFrame(Mat input){
+        Imgproc.cvtColor(input, modImg, Imgproc.COLOR_RGB2YCrCb);
+
+        Imgproc.rectangle(input,leftHalf,new Scalar(0, 255, 0),2);
+        Imgproc.rectangle(input,rightHalf,new Scalar(0, 255, 0),2);
+
+//        Imgproc.blur(modImg,blurredImg,new Size(15,15));
+
+        leftCrop = input.submat(leftHalf);
+        rightCrop = input.submat(rightHalf);
+
+        Core.extractChannel(leftCrop, leftCrop,2);
+        Core.extractChannel(leftCrop, leftCrop,2);
+
+        Scalar leftAvg = Core.mean(leftCrop);
+        Scalar rightAvg = Core.mean(rightCrop);
+
+        leftAvgFin = leftAvg.val[0];
+        rightAvgFin = rightAvg.val[0];
+//        0 is left, 1 is right
+
+        if(leftAvgFin > rightAvgFin){
+            location = 0;
+        } else if (rightAvgFin > leftAvgFin) {
+            location = 1;
+        }
+
+        return(rightCrop);
+
+    }
+
+
+}
