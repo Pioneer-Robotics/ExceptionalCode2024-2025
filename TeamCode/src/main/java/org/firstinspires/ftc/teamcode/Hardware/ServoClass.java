06b614e435bacd93f3434467e840bcd04d069b72
Braeden <braedeng2008@gmail.com>
2024-06-24 12:39:46 -0700

    Updated servo class with new methods and docstrings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ServoClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/ServoClass.java
@@ -5,12 +5,22 @@ package org.firstinspires.ftc.teamcode.Hardware;
  */
 
 import com.qualcomm.robotcore.hardware.Servo;
-import org.firstinspires.ftc.teamcode.Config;
 
 public class ServoClass {
     private final Servo servo;
-    private double openPos, closePos, pos1, pos2, pos3, pos4;
+    private final double closePos, openPos;
+    private double pos1;
+    private double pos2;
+    private double pos3;
+    private double pos4;
 
+    /**
+     * Constructor for ServoClass
+     *
+     * @param servo    Servo object
+     * @param openPos  double open position in range [0, 1]
+     * @param closePos double close position in range [0, 1]
+     */
     public ServoClass(Servo servo, double openPos, double closePos){
         this.servo = servo;
         this.openPos = openPos;
@@ -29,15 +39,53 @@ public class ServoClass {
 
     /**
      * Set the servo to any value
+     * @param pos double position in range [0, 1]
      */
     public void anyPos(double pos){servo.setPosition(pos);}
     /**
      * Set the servo to a preset
+     * @param pos int position in range [1, 4]
      */
     public void selectPos(int pos){
         if(pos == 1){servo.setPosition(pos1);}
         else if(pos == 2){servo.setPosition(pos2);}
         else if(pos == 3){servo.setPosition(pos3);}
-        else if(pos == 4){servo.setPosition(pos4);}
+        else if(pos == 4){servo.setPosition(pos4);
+        }
     }
+
+    /**
+     * Set servo preset position 1
+     *
+     * @param pos double position in range [0, 1]
+     */
+    public void setPos1(double pos) {
+        pos1 = pos;
+    }
+
+    /**
+     * Set servo preset position 2
+     *
+     * @param pos double position in range [0, 1]
+     */
+    public void setPos2(double pos) {
+        pos2 = pos;
+    }
+
+    /**
+     * Set servo preset position 3
+     *
+     * @param pos double position in range [0, 1]
+     */
+    public void setPos3(double pos) {
+        pos3 = pos;
+    }
+
+    /**
+     * Set servo preset position 4
+     *
+     * @param pos double position in range [0, 1]
+     */
+    public void setPos4(double pos) {
+        pos4 = pos;}
 }
