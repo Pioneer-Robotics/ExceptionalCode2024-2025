b93cb698e62f68ffc04ec6fa85b50917705b22f9
milopeters <milo_peters@icloud.com>
2024-08-18 13:03:02 -0700

    N degree Bezier curve (untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helpers/BezierCalc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helpers/BezierCalc.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Helpers;
 
 
+
 public class BezierCalc {
 
     public double[][] linearBez(double[] p0, double[] p1, int samples) {
@@ -31,4 +32,27 @@ public class BezierCalc {
         }
         return(points);
     }
+
+    public double[][] nDegBez(double[] px, double[] py, int samples) throws Exception {
+        if (px.length != py.length) {
+            throw new Exception("X coords do not math Y coords");
+        }
+        double[][] points = new double[samples][2];
+        int degree = px.length - 1;
+
+        for (int i = 0; i < samples; i++) {
+            double t = (double) i / (samples - 1);
+            double pointX = 0;
+            double pointY = 0;
+            for (int j = 0; j <= degree; j++) {
+                pointX += Utils.choose(degree, j) * (Math.pow(1 - t, degree - j)) * (Math.pow(t, j)) * px[j];
+                pointY += Utils.choose(degree, j) * (Math.pow(1 - t, degree - j)) * (Math.pow(t, j)) * py[j];
+            }
+            points[i][0] = pointX;
+            points[i][1] = pointY;
+        }
+        return (points);
+    }
+
+
 }
