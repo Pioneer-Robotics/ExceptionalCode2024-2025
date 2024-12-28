f754c06a58b4ebc7611f9cd9e8172b778c3d6342
Braeden <braedeng2008@gmail.com>
2024-12-28 12:17:27 -0700

    Added current monitoring thread to a schedule

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/CurrentDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/CurrentDetection.java
@@ -8,34 +8,31 @@ import org.firstinspires.ftc.teamcode.Config;
 import org.firstinspires.ftc.teamcode.Helpers.Utils;
 import org.slf4j.helpers.Util;
 
-public class CurrentDetection implements Runnable {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CurrentDetection {
     private volatile double current;
     private final double maxCurrent;
     private final DcMotorEx motor;
-    private boolean shutDown;
+    Timer timer;
+    TimerTask task;
     public CurrentDetection(DcMotorEx motor, double maxCurrent) {
         this.motor = motor;
-        // Just in case
         this.current = Float.POSITIVE_INFINITY;
         this.maxCurrent = maxCurrent;
-        this.shutDown = false;
     }
 
     public CurrentDetection(DcMotorEx motor) {
-        this.motor = motor;
-        this.current = Float.POSITIVE_INFINITY;
-        this.maxCurrent = Config.defaultMaxCurrent;
-        this.shutDown = false;
+        this(motor, Config.defaultMaxCurrent);
     }
 
-    public void run() {
-        while (!shutDown || Bot.opMode.isStopRequested()) {
-            current = motor.getCurrent(CurrentUnit.MILLIAMPS);
-            if (current > maxCurrent) {
-                Bot.opMode.telemetry.addLine("MOTOR REACHED MAX CURRENT");
-                Bot.opMode.gamepad1.rumble(1000);
-                Bot.opMode.gamepad2.rumble(1000);
-            }
+    public void checkCurrent() {
+        current = motor.getCurrent(CurrentUnit.MILLIAMPS);
+        if (current > maxCurrent) {
+            Bot.opMode.telemetry.addLine("MOTOR REACHED MAX CURRENT");
+            Bot.opMode.gamepad1.rumble(500);
+            Bot.opMode.gamepad2.rumble(500);
         }
     }
 
@@ -44,12 +41,17 @@ public class CurrentDetection implements Runnable {
     }
 
     public void stop() {
-        shutDown = true;
+        timer.cancel();
     }
-}
 
-class MaxCurrentException extends Exception {
-    public MaxCurrentException(String message) {
-        super(message);
+    public void start() {
+        this.timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                checkCurrent();
+            }
+        };
+        timer.schedule(task, 0, 500);
     }
 }
