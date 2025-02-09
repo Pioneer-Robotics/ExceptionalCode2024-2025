9c079071cc13fe295e434e339f2164c3fea66ae4
Gregg Goldner <twosuntraders@gmail.com>
2025-02-09 10:00:12 -0700

    Update for motor 1 and motor 2 for reset remove PIDFCoefficients code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Teleop/SlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Teleop/SlideTest.java
@@ -40,11 +40,6 @@ public class SlideTest extends LinearOpMode {
         motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // Set PDIF if necessary - this may not be needed
-        PIDFCoefficients pidf = new PIDFCoefficients(10, 3, 0, 0); // retune these values if necessary
-        motor1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
-        motor2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
-
         waitForStart();
 
         FtcDashboard dashboard = FtcDashboard.getInstance();
@@ -53,9 +48,12 @@ public class SlideTest extends LinearOpMode {
         while(opModeIsActive()) {
             if (gamepad1.a) {
                 // Reset the Encoder with the A button if issues arise
-                motor.setPower(0);  // Stop the motor
-                motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motor1.setPower(0);  // Stop the motor
+                motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motor2.setPower(0);  // Stop the motor
+                motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             } else {
                 // Look for up or down
                 if (gamepad1.dpad_up) {
@@ -73,13 +71,7 @@ public class SlideTest extends LinearOpMode {
             // Log motors current positions
             dashboardTelemetry.addData("Encoder Position 1", motor1.getCurrentPosition());
             dashboardTelemetry.addData("Encoder Position 2", motor2.getCurrentPosition());
-
-            // Log PIDF
-            PIDFCoefficients pidf1 = motor1.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
-            dashboardTelemetry.addData("PIDF 1", pidf1);
-            PIDFCoefficients pidf2 = motor2.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
-            dashboardTelemetry.addData("PIDF 2", pidf2);
-
+            
             // Log additional related values
             dashboardTelemetry.addData("Motor Velocity 1", motor1.getVelocity());
             dashboardTelemetry.addData("Motor Direction 1", motor1.getDirection());
