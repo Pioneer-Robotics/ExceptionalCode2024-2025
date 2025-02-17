d9972f0495f054e1273a1fdbafe81316abb7cc35
Michael Gross <mgstinger521@gmail.com>
2025-02-17 01:48:10 -0700

    Update LocatorClass.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/LocatorClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/LocatorClass.java
@@ -66,25 +66,14 @@ public class LocatorClass {
 
         blobs = colorLocator.getBlobs();
         //Check if list is empty, because the initial frame isn't processed, which can cause null pointer errors
-        if (blobs.isEmpty()){ {
+        if (blobs.toArray().length == 0) {
             blobs = colorLocator.getBlobs();
         }
         //Filter out very small blobs
         ColorBlobLocatorProcessor.Util.filterByArea(50, 80000, blobs);
         //Sorts blobs
         ColorBlobLocatorProcessor.Util.sortByArea(SortOrder.DESCENDING, blobs); // Largest blob is first, which is ideally the sample
-
-        // Sort blobs by closest to center of screen
-        Point center = new Point(streamWidth/2, streamHeight/2); // Based on Camera Properties
-
-        blobs.sort((o1, o2) -> {
-            if(o1.getBoxFit() == null || o2.getBoxFit() == null) return 0;
-
-            double d1 = pointDistance(o1.getBoxFit().center, center);
-            double d2 = pointDistance(o2.getBoxFit().center, center);
-            return Double.compare(d1, d2); // sort in ascending order of distance from center
-        });
-
+        //TODO: NEED TO SORT BY CLoSEST TO CENTER OF SCREEN
         return (blobs);
     }
 
